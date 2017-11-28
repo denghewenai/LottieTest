5a42e1c9a3c7c54844fb4aa878a3527087e721e9
denghewenai <code_funny@foxmail.com>
2017-11-28 13:16:40 +0800

    commit log info

--- app/src/main/java/com/example/cvte/lottietest/TextUseActivity.java
+++ app/src/main/java/com/example/cvte/lottietest/TextUseActivity.java
@@ -1,8 +1,18 @@
 package com.example.cvte.lottietest;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by CVTE on 2017/11/24.
  */
 
-public class TextUseActiivty {
+public class TextUseActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_use);
+    }
 }
